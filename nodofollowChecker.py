b738b8b322cb89098625cee4c3ea65b7d1d82698
Alisha <alishakamat8@gmail.com>
2022-02-08 19:35:36 +0530

    Update nodofollowChecker.py

--- nodofollowChecker.py
+++ nodofollowChecker.py
@@ -1,10 +1,10 @@
-import re #regex
-import urllib.request #URL handling module; used to fetch URLs
-from bs4 import BeautifulSoup #for parsing html/xml docs. used to extract data from HTML; useful for web scraping
+import re 
+import urllib.request 
+from bs4 import BeautifulSoup  
 
-page = urllib.request.urlopen('https://en.wikipedia.org/wiki/Main_Page')
+page = urllib.request.urlopen('https://demosite.com/about')
 html = BeautifulSoup(page.read(),"html.parser")
-links = html.find_all('a', attrs={"rel": "nofollow", "href": re.compile(r'wiki/')}) #"/wiki/Special:RecentChangesLinked/Main_Page"})
+links = html.find_all('a', attrs={"rel": "nofollow", "href": re.compile(r'wiki/')}) 
 for link in links:
     print(link)
-#re.compile(r'crummy\.com/')
+#re.compile(r'example\.com/')
